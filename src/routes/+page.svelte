cca8c7f365b9936b774d7346ef123d18d3bd10a9
Michael McKain <38301677+mikemckain@users.noreply.github.com>
2025-12-12 13:47:12 -0500

    dark mode test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,14 @@
 	let { data }: { data: PageData } = $props();
 </script>
 
-<h1>Posts</h1>
+<main class="max-w-3xl mx-auto px-6 py-12">
+	<h1 class="text-4xl font-bold mb-8">Posts</h1>
 
-<ul>
-	{#each data.posts as post}
-		<li>
-			<a href="/{post.slug}">{post.title}</a>
-		</li>
-	{/each}
-</ul>
+	<ul class="space-y-4">
+		{#each data.posts as post}
+			<li class="border-b border-neutral-800 pb-4">
+				<a href="/{post.slug}" class="text-xl hover:underline">{post.title}</a>
+			</li>
+		{/each}
+	</ul>
+</main>
